e0e74860d34d44ab823ea347e22c9cfe35d32e5a
Avanishpr <avanishavi@gmail.com>
2017-01-24 12:40:33 +0100

    initial android add

---
+++ Android/RunCarRun/app/src/main/java/com/mca/RoboBot/screen2.java
@@ -0,0 +1,14 @@
+package com.mca.RoboBot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+
+    }
+}
